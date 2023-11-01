1a405ac1130e1feb6737ae62d896cb488dfb371e
maricruz30 <114113923+maricruz30@users.noreply.github.com>
2023-10-31 18:30:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function muestra_oculta(){
+
+    if (screen.width > 960){
+        img.width = 150;
+    }
+  
+
+}
+
+
+function cambiaTamanio(caja){
+    logo = document.getElementById(caja);
+    logo.width = 200;
+    logo.height = 200;
+  }
